44ef75a001a835e97e69c0f51b80a8f20f8c8f5c
Aleksei Golubkov <nuriver@gmail.com>
2024-01-03 20:51:09 +0100

    feat: finish getIntervalArray task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -20,8 +20,12 @@
  *    getIntervalArray(0, 100) => [ 0, 1, 2, ..., 100 ]
  *    getIntervalArray(3, 3) => [ 3 ]
  */
-function getIntervalArray(/* start, end */) {
-  throw new Error('Not implemented');
+function getIntervalArray(start, end) {
+  const arr = Array.from(
+    { length: end - start + 1 },
+    (val, i) => start + i * 1
+  );
+  return arr;
 }
 
 /**
