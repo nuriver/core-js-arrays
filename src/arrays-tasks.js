adadc11688d8dcf24edaf3a889a4a08189286356
Aleksei Golubkov <nuriver@gmail.com>
2024-01-03 23:05:20 +0100

    feat: finish insertItem task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -196,8 +196,11 @@ function isValueEqualsIndex(arr) {
  *    insertItem([ 1, 3, 4, 5 ], 2, 1)  => [ 1, 2, 3, 4, 5 ]
  *    insertItem([ 1, 'b', 'c'], 'x', 0) => [ 'x', 1, 'b', 'c' ]
  */
-function insertItem(/* arr, item, index */) {
-  throw new Error('Not implemented');
+function insertItem(arr, item, index) {
+  const spliceArr = arr.splice(index);
+  arr.push(item);
+  arr.push(...spliceArr);
+  return arr;
 }
 
 /**
