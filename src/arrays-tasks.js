b77b76c19ead6cb250ef8f91a34fbfd4c7686b96
Aleksei Golubkov <nuriver@gmail.com>
2024-01-04 21:24:33 +0100

    feat: finish getFalsyValuesCount task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -424,8 +424,10 @@ function getElementByIndices(arr, indices) {
  *  getFalsyValuesCount([ -1, 'false', null, 0 ]) => 2
  *  getFalsyValuesCount([ null, undefined, NaN, false, 0, '' ]) => 6
  */
-function getFalsyValuesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getFalsyValuesCount(arr) {
+  const falsArr = [false, null, 0, '', undefined, NaN];
+  const finalArr = arr.filter((elem) => falsArr.includes(elem));
+  return finalArr.length;
 }
 
 /**
