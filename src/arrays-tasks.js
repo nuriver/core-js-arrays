1321b40a25899dc52a259511209b86bf5ec0bea7
Aleksei Golubkov <nuriver@gmail.com>
2024-01-05 11:19:33 +0100

    feat: finish sortDigitNamesByNumericOrder task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -625,8 +625,58 @@ function shiftArray(arr, n) {
  *   sortDigitNamesByNumericOrder([ 'nine','eight','nine','eight' ]) => [ 'eight','eight','nine','nine']
  *   sortDigitNamesByNumericOrder([ 'one','one','one','zero' ]) => [ 'zero','one','one','one' ]
  */
-function sortDigitNamesByNumericOrder(/* arr */) {
-  throw new Error('Not implemented');
+function sortDigitNamesByNumericOrder(arr) {
+  const numericArr = arr
+    .map((elem) => {
+      switch (elem) {
+        case 'one':
+          return 1;
+        case 'two':
+          return 2;
+        case 'three':
+          return 3;
+        case 'four':
+          return 4;
+        case 'five':
+          return 5;
+        case 'six':
+          return 6;
+        case 'seven':
+          return 7;
+        case 'eight':
+          return 8;
+        case 'nine':
+          return 9;
+        default:
+          return 0;
+      }
+    })
+    .sort((a, b) => a - b);
+
+  return numericArr.map((elem) => {
+    switch (elem) {
+      case 1:
+        return 'one';
+      case 2:
+        return 'two';
+      case 3:
+        return 'three';
+      case 4:
+        return 'four';
+      case 5:
+        return 'five';
+      case 6:
+        return 'six';
+      case 7:
+        return 'seven';
+      case 8:
+        return 'eight';
+      case 9:
+        return 'nine';
+      default:
+        return 'zero';
+    }
+  });
 }
 
 /**
